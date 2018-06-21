eaf6c7e193cb3db38c1b62ecb58bacc7862853aa
Matt Bacher <mattjb1991@sbcglobal.net>
2018-06-21 18:53:52 -0400

    Initial commit with Spring initialized, maven for bookstore

---
+++ src/main/java/com/JavaBookstore/JavaBookstore/JavaBookstoreApplication.java
@@ -0,0 +1,12 @@
+package com.JavaBookstore.JavaBookstore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaBookstoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaBookstoreApplication.class, args);
+	}
+}
